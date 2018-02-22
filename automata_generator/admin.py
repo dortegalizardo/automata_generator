24d0f4d52f874f43c9939a99560ce9f3a239fc5f
dortegalizardo <douglas@e-libro.com>
2018-02-22 03:44:43 -0600

    Registro de modelos en el sitio administrativo.

--- automata_generator/admin.py
+++ automata_generator/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from automata_generator.models import (
+    Automata,
+    AutomataState,
+    AutomataLanguage,
+    AutomataTransition,
+    AutomataTest
+)
 
 # Register your models here.
+
+admin.site.register(Automata)
+admin.site.register(AutomataState)
+admin.site.register(AutomataLanguage)
+admin.site.register(AutomataTransition)
+admin.site.register(AutomataTest)
